0c8119ddb97f3ac381ca739d51974b2f59cafd7d
msrodriguez <msrodriguez@crimson.ua.edu>
2014-03-06 15:25:50 -0600

    Basic button/strings test

---
+++ RECRUTRAK5000/src/com/example/recrutrak5000/StudentLogin.java
@@ -0,0 +1,22 @@
+package com.example.recrutrak5000;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+
+public class StudentLogin extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.student_login);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
