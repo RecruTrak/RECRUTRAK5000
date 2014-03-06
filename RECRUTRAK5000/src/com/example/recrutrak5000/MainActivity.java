8a9f5285ebf9d35fd3def37eee3bf7e8761880b6
msrodriguez <msrodriguez@crimson.ua.edu>
2014-03-06 15:52:48 -0600

    Intents to change activity

--- RECRUTRAK5000/src/com/example/recrutrak5000/MainActivity.java
+++ RECRUTRAK5000/src/com/example/recrutrak5000/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.recrutrak5000;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +13,16 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		final Button button = (Button) findViewById(R.id.button1);
+		button.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(MainActivity.this, StudentLogin.class);
+				startActivity(intent);
+				
+			}
+		});
 	}
 
 	@Override
