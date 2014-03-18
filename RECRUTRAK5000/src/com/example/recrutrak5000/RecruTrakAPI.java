1f6a754ec304cc46dd7c4c2b5e5bad1b93c60887
Brian <mac.gamer@gmail.com>
2014-03-18 13:38:46 -0500

    java api updates

--- RECRUTRAK5000/src/com/example/recrutrak5000/RecruTrakAPI.java
+++ RECRUTRAK5000/src/com/example/recrutrak5000/RecruTrakAPI.java
@@ -1,19 +1,26 @@
 package com.example.recrutrak5000;
 
+import retrofit.RestAdapter;
 import retrofit.http.*;
 
 public class RecruTrakAPI {
-	private static final String API_URL = "http://recrutrak5000-macamatic.rhcloud.com/api";
-
-	static class Session {
-		String message;
+	private static final String url = "http://recrutrak5000-macamatic.rhcloud.com/api";
+	private static final RecruTrak api = new RestAdapter.Builder().setEndpoint(url).build().create(RecruTrak.class);
+	
+	static class LoginResult {
+		boolean success;
 	}
 
 	interface RecruTrak {
 		@POST("/login/username/{username}/password/{password}")
-		Session login(
+		LoginResult login(
 			@Path("username") String username,
 			@Path("password") String password
 		);
+	}
+
+	public boolean login(String username, String password) {
+		LoginResult result = api.login(username, password);
+		return result.success;
 	}
 }
