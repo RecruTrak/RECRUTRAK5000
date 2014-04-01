e01f05928808c2182fb99e43ce3ccfbdf53457f3
Brian <mac.gamer@gmail.com>
2014-04-01 14:13:58 -0500

    a few api updates

--- server/php/application/controllers/api.php
+++ server/php/application/controllers/api.php
@@ -14,6 +14,31 @@
 			}
 		}
 
+		public function students_get() {
+			$student = $this->db->get_where('students', $this->get())->row();
+			if (!empty($student)) {
+				$this->response($student);
+			} else {
+				$this->response(false, 404);
+			}
+		}
+
+		public function students_put() {
+			if ($this->db->update('students', $this->put(), array('id' => $this->get('id')))) {
+				$this->response(true, 202);
+			} else {
+				$this->response(false, 404);
+			}
+		}
+
+		public function students_post() {
+			if ($this->db->insert('students', $this->post())) {
+				$this->response($this->db->insert_id(), 201);
+			} else {
+				$this->response(false, 403);
+			}
+		}
+
 	}
 
 ?>
