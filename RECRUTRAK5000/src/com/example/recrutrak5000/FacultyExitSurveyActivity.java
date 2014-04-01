4f49a2127e9fc23199ae5a46bbd9efae1ab6dc19
msrodriguez <msrodriguez@crimson.ua.edu>
2014-04-01 18:08:20 -0500

    Exit survey layouts, activities

--- RECRUTRAK5000/src/com/example/recrutrak5000/FacultyExitSurveyActivity.java
+++ RECRUTRAK5000/src/com/example/recrutrak5000/FacultyExitSurveyActivity.java
@@ -4,12 +4,12 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 
-public class ExitSurveyActivity extends Activity {
+public class FacultyExitSurveyActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.exit_survey_activity);
+		setContentView(R.layout.student_exit_survey_activity);
 		
 		
 	}
