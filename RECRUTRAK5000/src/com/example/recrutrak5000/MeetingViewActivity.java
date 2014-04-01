149ab79f22195a61d1a83ff5d870f09fe7078dbb
Niklas Fahl <niklas.fahl@me.com>
2014-04-01 16:37:43 -0500

    fixed errors

--- RECRUTRAK5000/src/com/example/recrutrak5000/MeetingViewActivity.java
+++ RECRUTRAK5000/src/com/example/recrutrak5000/MeetingViewActivity.java
@@ -8,26 +8,26 @@ import android.os.Bundle;
 @TargetApi(Build.VERSION_CODES.HONEYCOMB)
 public class MeetingViewActivity extends Activity {
 	
-	static final LatLng Comer = new LatLng(33.2155727, -87.5442692);
-	//static final LatLng KIEL = new LatLng(53.551, 9.993);
-	private GoogleMap map;
-
-	  @Override
-	  protected void onCreate(Bundle savedInstanceState) {
-	    super.onCreate(savedInstanceState);
-	    setContentView(R.layout.activity_meeting_view);
-	    map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map))
-	        .getMap();
-	    
-	    if (map!=null){
-	      Marker comer = map.addMarker(new MarkerOptions().position(Comer)
-	          .title("Comer"));
-	      /*Marker kiel = map.addMarker(new MarkerOptions()
-	          .position(KIEL)
-	          .title("Kiel")
-	          .snippet("Kiel is cool")
-	          .icon(BitmapDescriptorFactory
-	              .fromResource(R.drawable.ic_launcher)));*/
-	    }
-	  }   
+//	static final LatLng Comer = new LatLng(33.2155727, -87.5442692);
+//	//static final LatLng KIEL = new LatLng(53.551, 9.993);
+//	private GoogleMap map;
+//
+//	  @Override
+//	  protected void onCreate(Bundle savedInstanceState) {
+//	    super.onCreate(savedInstanceState);
+//	    setContentView(R.layout.activity_meeting_view);
+//	    map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map))
+//	        .getMap();
+//	    
+//	    if (map!=null){
+//	      Marker comer = map.addMarker(new MarkerOptions().position(Comer)
+//	          .title("Comer"));
+//	      /*Marker kiel = map.addMarker(new MarkerOptions()
+//	          .position(KIEL)
+//	          .title("Kiel")
+//	          .snippet("Kiel is cool")
+//	          .icon(BitmapDescriptorFactory
+//	              .fromResource(R.drawable.ic_launcher)));*/
+//	    }
+//	  }   
 }
