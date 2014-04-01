4484ea08eeef7ae0aded938fb15b11de80d0b636
msrodriguez <msrodriguez@crimson.ua.edu>
2014-04-01 18:46:19 -0500

    Fixed admin login, surveys update

--- RECRUTRAK5000/src/com/example/recrutrak5000/FacultyExitSurveyActivity.java
+++ RECRUTRAK5000/src/com/example/recrutrak5000/FacultyExitSurveyActivity.java
@@ -1,8 +1,11 @@
 package com.example.recrutrak5000;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class FacultyExitSurveyActivity extends Activity {
 
@@ -11,7 +14,14 @@ public class FacultyExitSurveyActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.student_exit_survey_activity);
 		
-		
+		final Button meetingView = (Button) findViewById(R.id.button1);
+		meetingView.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				
+				finish();
+			}
+		});
 	}
 
 	@Override
