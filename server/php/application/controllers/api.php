ca79505fbb34be46bb55a0454a064067005c6104
Brian <mac.gamer@gmail.com>
2014-04-01 16:26:43 -0500

    api stuff

--- server/php/application/controllers/api.php
+++ server/php/application/controllers/api.php
@@ -1,5 +1,6 @@
-<?php require_once(APPPATH . 'libraries/REST_Controller.php');
+<?php
 
+	require_once(APPPATH . 'libraries/REST_Controller.php');
 	ini_set('session.use_cookies', '0');
 
 	class Api extends REST_Controller {
@@ -8,23 +9,25 @@
 			$_SESSION = array();
 			$user = $this->db->get_where('users', $this->get())->row();
 			if (!empty($user)) {
-				$this->response(true);
+				$this->response(true, 200);
 			} else {
 				$this->response(false, 403);
 			}
 		}
 
-		public function students_get() {
-			$student = $this->db->get_where('students', $this->get())->row();
+		public function students_get($id) {
+			$student = $this->db->get_where('students', array('id' => $id))->row();
 			if (!empty($student)) {
-				$this->response($student);
+				$this->response($student, 200);
 			} else {
 				$this->response(false, 404);
 			}
 		}
 
-		public function students_put() {
-			if ($this->db->update('students', $this->put(), array('id' => $this->get('id')))) {
+		public function students_put($id) {
+			$student = $this->put();
+			unset($student['id']);
+			if ($this->db->update('students', $student, array('id' => $id))) {
 				$this->response(true, 202);
 			} else {
 				$this->response(false, 404);
@@ -32,7 +35,9 @@
 		}
 
 		public function students_post() {
-			if ($this->db->insert('students', $this->post())) {
+			$student = $this->post();
+			unset($student['id']);
+			if ($this->db->insert('students', $student)) {
 				$this->response($this->db->insert_id(), 201);
 			} else {
 				$this->response(false, 403);
