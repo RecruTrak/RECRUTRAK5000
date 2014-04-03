f5427d6faaf477777f1d0d28a9a93b20dfc64b6e
Niklas Fahl <niklas.fahl@me.com>
2014-04-03 16:27:38 -0500

    some request visit stuff

--- RECRUTRAK5000/src/com/example/recrutrak5000/RequestVisitActivity.java
+++ RECRUTRAK5000/src/com/example/recrutrak5000/RequestVisitActivity.java
@@ -3,13 +3,29 @@ package com.example.recrutrak5000;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class RequestVisitActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+		
+		
 		setContentView(R.layout.request_visit_activity);
+		
+		String colors[] = {"Choose...","Senior","Junior","Sophomore","Freshman", "Transfer Student"};
+
+		// Selection of the spinner
+		Spinner spinner = (Spinner) findViewById(R.id.spYearInSchool);
+		
+
+		// Application of the Array to the Spinner
+		ArrayAdapter<String> spinnerArrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, colors);
+		spinnerArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item); // The drop down view
+		spinner.setAdapter(spinnerArrayAdapter);
 	}
 
 	@Override
