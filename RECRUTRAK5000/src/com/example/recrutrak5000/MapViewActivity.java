adc050f48c5fcb4d2eaeae3c1a08cd2639ade254
msrodriguez <msrodriguez@crimson.ua.edu>
2014-04-03 17:11:39 -0500

    Moved Map, working on post login menus

--- RECRUTRAK5000/src/com/example/recrutrak5000/MapViewActivity.java
+++ RECRUTRAK5000/src/com/example/recrutrak5000/MapViewActivity.java
@@ -13,16 +13,15 @@ import android.os.Build;
 import android.os.Bundle;
 
 @TargetApi(Build.VERSION_CODES.HONEYCOMB)
-public class MeetingViewActivity extends Activity {
+public class MapViewActivity extends Activity {
 	
 	static final LatLng hmComer = new LatLng(33.2155727, -87.5442692);
-	//static final LatLng KIEL = new LatLng(53.551, 9.993);
 	private GoogleMap map;
 
 	  @Override
 	  protected void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
-	    setContentView(R.layout.activity_meeting_view);
+	    setContentView(R.layout.map_view_activity);
 	    map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map))
 	        .getMap();
 	    
@@ -32,12 +31,6 @@ public class MeetingViewActivity extends Activity {
 	          .snippet("Meeting room in nnn room at tt:tt"));
 	      
 	      map.moveCamera(CameraUpdateFactory.newLatLngZoom(hmComer, 17));
-	      /*Marker kiel = map.addMarker(new MarkerOptions()
-	          .position(KIEL)
-	          .title("Kiel")
-	          .snippet("Kiel is cool")
-	          .icon(BitmapDescriptorFactory
-	              .fromResource(R.drawable.ic_launcher)));*/
 	    }
 	  }   
 }
