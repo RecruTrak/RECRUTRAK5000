7e4462e67e1fd603ae83ee5b6579e9697f9cf22d
msrodriguez <msrodriguez@crimson.ua.edu>
2014-04-03 20:35:29 -0500

    Fixed some stuff

---
+++ RECRUTRAK5000/src/com/example/recrutrak5000/StaffMainActivity.java
@@ -0,0 +1,53 @@
+package com.example.recrutrak5000;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+
+public class StaffMainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.staff_main_activity);
+		final Button setAvailability = (Button) findViewById(R.id.button1);
+		setAvailability.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(StaffMainActivity.this, SetAvailabilityActivity.class);
+				startActivity(intent);
+			}
+		});
+		
+		final Button viewMeetings = (Button) findViewById(R.id.button2);
+		viewMeetings.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(StaffMainActivity.this, ViewMeetingsActivity.class);
+				startActivity(intent);
+			}
+		});
+		
+		final Button logout = (Button) findViewById(R.id.button3);
+		logout.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				finish();
+			}
+		});
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
